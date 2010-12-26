211d96a6e35ccb1037b40557cea85d461cea1f7d
Sean Camden <sean@seancamden.com>
2010-12-25 21:53:02 -0800

    Added tracking to the mobile section.

--- index.php
+++ index.php
@@ -238,6 +238,18 @@ piwikTracker.enableLinkTracking();
 </head>
 <body>
 <p>Sorry, I know mobile's all the rage these days, but the Camden Xmas card is just too heavy for your little device. But Merry Xmas anyways!</p>
+<!-- Piwik -->
+<script type="text/javascript">
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://www.seancamden.com/stats/piwik/" : "http://www.seancamden.com/stats/piwik/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 2);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://www.seancamden.com/stats/piwik/piwik.php?idsite=2" style="border:0" alt="" /></p></noscript>
+<!-- End Piwik Tag -->
 </body>
 </html>
 <?php } ?>
