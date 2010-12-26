2cb153428830928fc149a47d0291549508cd9621
Sean Camden <sean@seancamden.com>
2010-12-26 12:59:17 -0800

    Added links to full-size images to the small_device page.

--- small_device.php
+++ small_device.php
@@ -8,21 +8,22 @@
 <body>
 <div id="mycontent">
 <p>It seems you've arrived here via a phone of some sort. That's cool. I dig where you're coming from. It's just that the card is all jammed full of 
-  pictures and movies, so you'd just run through your monthly quota before actually seeing anything. So, I'll just stack the images up here so you can see
+  pictures and movies, so you'd just run through your monthly quota before actually seeing anything. So, I'll just stack the reduced images up here so you can see
   them, and I'll list the youtube links for the videos below. There's a much nicer presentation if you come back while on a device with a proper sized monitor.</p>
-<img src="images/IMG_5281.jpg" width="800" height="300" alt="Daniela, Shannon, and Mommy" title="Daniela, Shannon, and Mommy" /><br />
-<img src="images/2010-12-15-16.11.54.jpg" width="800" height="300" alt="Daniela" title="Daniela" /><br />
-<img src="images/2010-12-20-14.38.18.jpg" width="800" height="300" alt="Shannon" title="Shannon" /><br />
-<img src="images/IMG_5325.jpg" width="800" height="300" alt="Daniela" title="Daniela" /><br />
-<img src="images/IMG_5252.jpg" width="800" height="300" alt="Shannon" title="Shannon" /><br />
-<img src="images/IMG_4787.jpg" width="800" height="300" alt="Daddy" title="Daddy" /><br />
-<img src="images/IMG_5260.jpg" width="800" height="300" alt="Shannon" title="Shannon" /><br />
+<a href="images/IMG_5281.jpg"><img src="images/mobile/IMG_5281.jpg" width="400" height="150" alt="Daniela, Shannon, and Mommy" title="Daniela, Shannon, and Mommy" /></a><br />
+<a href="images/2010-12-15-16.11.54.jpg"><img src="images/mobile/2010-12-15-16.11.54.jpg" width="400" height="150" alt="Daniela" title="Daniela" /></a><br />
+<a href="images/2010-12-20-14.38.18.jpg"><img src="images/mobile/2010-12-20-14.38.18.jpg" width="400" height="150" alt="Shannon" title="Shannon" /></a><br />
+<a href="images/IMG_5325.jpg"><img src="images/mobile/IMG_5325.jpg" width="400" height="150" alt="Daniela" title="Daniela" /></a><br />
+<a href="images/IMG_5252.jpg"><img src="images/mobile/IMG_5252.jpg" width="400" height="150" alt="Shannon" title="Shannon" /></a><br />
+<a href="images/IMG_4787.jpg"><img src="images/mobile/IMG_4787.jpg" width="400" height="150" alt="Daddy" title="Daddy" /></a><br />
+<a href="images/IMG_5260.jpg"><img src="images/mobile/IMG_5260.jpg" width="400" height="150" alt="Shannon" title="Shannon" /></a><br />
 <a href="http://www.youtube.com/watch?v=zqWMaBYvS9s">Peter Pan's House </a><br />
 <a href="http://www.youtube.com/watch?v=qH8rU82Uc1Y">Truly Scrumptious </a><br />
 <a href="http://www.youtube.com/watch?v=n4OrYSbplA8">Bubbles in Arcata </a><br />
 <a href="http://www.youtube.com/watch?v=np61vWy0H1o">Good Night Song </a><br />
 <a href="http://www.youtube.com/watch?v=FdSc_TTgcv4">Must Be Santa </a><br />
 </div>
+<div style="position:absolute; bottom:0;" id="bottom_image">&nbsp;</div>
 <!-- Piwik -->
 <script type="text/javascript">
 var pkBaseURL = (("https:" == document.location.protocol) ? "https://www.seancamden.com/stats/piwik/" : "http://www.seancamden.com/stats/piwik/");
